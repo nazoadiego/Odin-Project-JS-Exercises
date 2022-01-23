7b42d760e2fe9ac27956f49bfaaf9062c3685c96
diegomarrufo <dmarrufo@ucm.es>
2022-01-24 00:28:21 +0100

    repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(message, times) {
+  let finalMessage = '';
+  for (let i = 0; i < times; i++) {
+    finalMessage += message;
+  };
+  if (times < 0) { finalMessage = "ERROR" }
+  return finalMessage;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
