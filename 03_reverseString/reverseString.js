2741b6f00f6010a7dcd24766620f1dc347b8fef8
diegomarrufo <dmarrufo@ucm.es>
2022-01-24 00:49:26 +0100

    reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
 const reverseString = function(str) {
-
+  const splittedStr = str.split("")
+  const reversedStr = splittedStr.reverse()
+  const joinedStr = reversedStr.join("")
+  return joinedStr
 };
 
 // Do not edit below this line
